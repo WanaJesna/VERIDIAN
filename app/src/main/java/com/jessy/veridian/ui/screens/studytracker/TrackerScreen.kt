ecfa0b8e49f6b3654ea8d2e0ec20cb2bec55f90d
Christine239 <wanajesna@gmail.com>
2025-05-06 16:12:58 +0300

    project commit

---
+++ app/src/main/java/com/jessy/veridian/ui/screens/studytracker/TrackerScreen.kt
@@ -0,0 +1,47 @@
+package com.jessy.veridian.ui.screens.studytracker
+
+
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+
+
+@Composable
+fun TrackerScreen(navController: NavController){
+
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TrackerScreenPreview(){
+    TrackerScreen(navController= rememberNavController())
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
